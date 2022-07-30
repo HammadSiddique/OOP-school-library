29d13644ec646bddd860b65f3fad0d94052dceca
HammadSiddique <hammad.siddique01@gmail.com>
2022-07-30 21:38:04 +0500

    Add classroom method in students.rb

--- person.rb
+++ person.rb
@@ -1,16 +1,17 @@
 require './nameable'
 
 class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
     super()
   end
-
-  attr_accessor :name, :age
-  attr_reader :id
 
   private
 
@@ -26,5 +27,9 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
   end
 end
--- student.rb
+++ student.rb
@@ -1,12 +1,18 @@
 require './person'
+require './classroom'
 
 class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission)
-    @classroom = classroom
+    @classroom = Classroom.new(classroom)
   end
 
   def play_hooky
     "¯\(ツ)/¯"
   end
+
+  def classroom(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+    end
 end
